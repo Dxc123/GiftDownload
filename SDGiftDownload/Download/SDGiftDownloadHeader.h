f323d2cc845b95eb2f64e6f1f97ab5a943d024a3
dxc_123 <daixingchuang@163.com>
2022-01-06 00:37:38 +0800

    update

---
+++ SDGiftDownload/Download/SDGiftDownloadHeader.h
@@ -0,0 +1,17 @@
+//
+//  SDGiftDownloadHeader.h
+//  SDGiftDownload
+//
+//  Created by daixingchuang on 2022/1/6.
+//
+
+#ifndef SDGiftDownloadHeader_h
+#define SDGiftDownloadHeader_h
+
+#import "SGCacheManager.h"
+#import "SGDownloadManager.h"
+#import "ReceivedGiftInfo.h"
+#import "SGDownloadModel.h"
+
+#import "SDGiftDownloadManager.h"
+#endif /* SDGiftDownloadHeader_h */
